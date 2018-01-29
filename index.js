665b576be9fb61f63c32c61cf1fc725628c2d941
Jeff Fowler <jeff.fowler@peakon.com>
2018-01-29 15:41:31 +0100

    dont need append

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const util = require('util');
 const fs = require('fs');
 
-module.exports = function (thing, append = false) {
-  if (append) {
-    fs.appendFile('/tmp/jfo.log', util.format('%o\n', thing));
-  } else {
-    fs.writeFile('/tmp/jfo.log', util.format('%o\n', thing));
-  }
+module.exports = function (thing, logpath = '/tmp/jfo.log') {
+  fs.writeFile(logpath, util.format('%o\n', thing));
 };
